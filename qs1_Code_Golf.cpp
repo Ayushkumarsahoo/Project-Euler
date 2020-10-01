9c5502cffa6ed77f72fb1962b7447868d99d6387
Abhinandan Roul <53302086+AbhinandanRoul@users.noreply.github.com>
2020-10-01 22:47:54 +0530

    Update qs1_Code_Golf.cpp

--- qs1_Code_Golf.cpp
+++ qs1_Code_Golf.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main()
-{
-    int i,sum=0;
+{int i,sum=0;
     while(++i <1000)
        sum+= i%3==0 || i%5==0? i:0;
-    cout<<sum;
-}
+    cout<<sum;}
